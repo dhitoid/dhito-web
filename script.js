1183eceadfe1168f91844d244023db156c1987f1
DT DEV ID <153781142+dhitoid@users.noreply.github.com>
2026-02-27 23:35:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const navbar = document.querySelector(".navbar");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 20) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener("click", function(e) {
     e.preventDefault();
