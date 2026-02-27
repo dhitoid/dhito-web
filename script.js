05978e439647192d2c299ab23f1aeb2c0e361d22
DT DEV ID <153781142+dhitoid@users.noreply.github.com>
2026-02-27 23:14:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,7 @@
-const cards = document.querySelectorAll(".card");
-
-cards.forEach(card => {
-  card.style.opacity = 0;
-  card.style.transform = "translateY(30px)";
-});
-
-window.addEventListener("scroll", () => {
-  cards.forEach(card => {
-    const top = card.getBoundingClientRect().top;
-    if (top < window.innerHeight - 50) {
-      card.style.opacity = 1;
-      card.style.transform = "translateY(0)";
-      card.style.transition = "0.6s ease";
-    }
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
   });
 });
-
-const slides = document.querySelector(".slides");
-const dots = document.querySelectorAll(".dot");
-
-let index = 0;
-
-function showSlide(i) {
-  slides.style.transform = `translateX(-${i * 100}%)`;
-
-  dots.forEach(dot => dot.classList.remove("active"));
-  dots[i].classList.add("active");
-}
-
-function nextSlide() {
-  index++;
-  if (index >= dots.length) {
-    index = 0;
-  }
-  showSlide(index);
-}
-
-setInterval(nextSlide, 4000);
