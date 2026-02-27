657136e9495542b4f24e09212c84a05d31f8292c
DT DEV ID <153781142+dhitoid@users.noreply.github.com>
2026-02-27 23:51:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,21 @@ const navbar = document.querySelector(".navbar");
       navbar.classList.remove("scrolled");
     }
   });
+  
+  const reveals = document.querySelectorAll(".reveal");
+
+  const revealOnScroll = () => {
+    const windowHeight = window.innerHeight;
+    reveals.forEach(el => {
+      const elementTop = el.getBoundingClientRect().top;
+      if (elementTop < windowHeight - 100) {
+        el.classList.add("active");
+      }
+    });
+  };
+
+  window.addEventListener("scroll", revealOnScroll);
+  revealOnScroll();
 
   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener("click", function(e) {
