0363f6df7d4e41128c5ed612812d2097804e7dd5
DT DEV ID <153781142+dhitoid@users.noreply.github.com>
2026-03-02 22:44:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -44,10 +44,10 @@ const reveals = document.querySelectorAll(".reveal");
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry, index) => {
     if(entry.isIntersecting){
-      entry.target.style.transitionDelay = `${index * 0.1}s`;
-      entry.target.classList.add("active");
-      observer.unobserve(entry.target);
-    }
+        entry.target.style.transitionDelay = `${entry.target.dataset.delay * 0.1}s`;
+        entry.target.classList.add("active");
+        observer.unobserve(entry.target);
+      }
   });
 },{
   threshold: 0.15
